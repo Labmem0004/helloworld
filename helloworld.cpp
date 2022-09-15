00574907a51e3974d652f2e4806651de039616f2
Leon <goodnight327@gmail.com>
2022-09-15 19:39:19 +0800

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    double a=0, b=0;
+
+    cin >> a >> b;
+    cout << "the sum is " << a+b;
+
+
+    return 0;
+}
