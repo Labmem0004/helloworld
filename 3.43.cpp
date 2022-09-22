37d33f04bf596f524e63a4cd1f935041c7de37c1
Leon <goodnight327@gmail.com>
2022-09-22 10:34:02 +0800

    指针与数组之间的关系

--- 3.43.cpp
+++ 3.43.cpp
@@ -5,25 +5,42 @@ int main() {
     //编写3个不同版本的程序，均能输出ia的元素。
     int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-    //版本1使用范围for语句管理迭代过程
+    //版本1使用范围for语句管理迭代过程，使用指针的版本
+    cout << "int *i : ia" << endl;
     for (int *i : ia) {
-        cout << i << '\0';
+        cout << *i << ' ';
+        i++;
+        cout << *i << ' ';
+        i++;
+        cout << *i << ' ';
+        i++;
+        cout << *i << '\n';
     }
     cout << '\n' << endl;
+    //版本1使用范围for，但是引用
+    cout << "int (&i)[4] : ia" << endl;
+    for (int (&i)[4] : ia) {
+        for (int &j : i) {
+            cout << j << ' ';
+        }
+        cout << '\n';
+    }
+    cout << endl;
 
     //版本2使用下标运算符
     for (size_t i=0; i<3; i++) {
         for (size_t j=0; j < 4; j++) {
-            cout << ia[i][j] << '\0';
+            cout << ia[i][j] << ' ';
         }
         cout << '\n';
     }
     cout << endl;
 
     //版本3使用指针
+    cout << "使用指针，但不用范围for" << endl;
     for (int (*row)[4] = ia; row<end(ia); row++) {
         for (int *col = *row; col<end(*row); col++) {
-            cout << *col << '\0';
+            cout << *col << ' ';
         }
         cout << '\n';
     }
